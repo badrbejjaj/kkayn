cc0b7ad68a389a47850a117b64d33a9b792f8f44
badrbejjaj <badrbejjaj@gmail.com>
2022-08-12 20:11:17 +0100

    feat(project-init-automation): automate project init

---
+++ init-project.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+sh update-tools.sh
+
+
+echo "Configuration de l'environnement frontend local"
+
+cd frontend/src/src/environments/
+
+cp -f environment.local.ts environment.ts
+
+cd -
+
+echo "Configuration de l'environnement backend local"
+
+cd backend/src/
+
+cp -f .env.local .env
+
+cd -
+
+echo "Installation des hooks GIT"
+
+git config core.hooksPath .githooks
+
+echo "Génération des APIs"
+
+makeapi --sourcesPath src/ -O --skip-validate-spec
